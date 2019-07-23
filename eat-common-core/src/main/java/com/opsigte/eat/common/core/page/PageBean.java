5426507eae5b44553209f84657833359bda4803b
opsigte <opsigte994@163.com>
2019-07-23 20:37:34 +0800

    更名之前

--- eat-common-core/src/main/java/com/opsigte/eat/common/core/page/PageBean.java
+++ eat-common-core/src/main/java/com/opsigte/eat/common/core/page/PageBean.java
@@ -14,7 +14,6 @@ import java.util.List;
  * @Date: 2019/7/16 23:01
  * @version 1.0.0
  */
-@Data
 public class PageBean<T> implements Serializable {
     private static final long serialVersionUID = 2046352945524278277L;
 
@@ -38,6 +37,46 @@ public class PageBean<T> implements Serializable {
      * 数据
      */
     private List<T> list;
+
+    public Integer getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(Integer currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public Integer getNumPerPage() {
+        return numPerPage;
+    }
+
+    public void setNumPerPage(Integer numPerPage) {
+        this.numPerPage = numPerPage;
+    }
+
+    public Integer getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(Integer totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    public List<T> getList() {
+        return list;
+    }
+
+    public void setList(List<T> list) {
+        this.list = list;
+    }
 
     /**
      * 将PageHelper分页后的list转为分页信息
